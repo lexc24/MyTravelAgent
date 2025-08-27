78cab5da5bcfebae4ac24c7b4012f6ac0ba16efc
lexc24 <129222152+lexc24@users.noreply.github.com>
2025-08-26 19:03:42 -0500

    Update settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -30,8 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY', default='your-current-secret-key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # Environment detection
-DEBUG = os.environ.get('DEBUG', default=False)
-
+DEBUG = os.environ.get('DEBUG', default=False, cast=bool)
 if DEBUG:
     ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 else:
